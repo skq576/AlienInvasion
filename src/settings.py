bf6bb8d7f9c83f3f7ea6ee40edc51ee7d3f0f316
Mark Sanchez <marksanchez@Marks-MacBook-Pro.local>
2020-01-13 15:24:47 -0600

    adding alien invasion and setting files

---
+++ src/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """ A class to sort all the settings for AI"""
+
+    def __init__(self):
+        """ Initialize game settings"""
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
